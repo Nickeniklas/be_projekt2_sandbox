748827fd26b70824978102e44314178b51e36a9e
Nickeniklas <savonhen@arcada.fi>
2023-02-23 20:40:34 +0200

    $stmt ändrad till $result i query methoden av SQL

--- projekt2/model_profiles.php
+++ projekt2/model_profiles.php
@@ -1,11 +1,11 @@
 <?php
 $sql = "SELECT * FROM profiles";
 
-$stmt = $conn->query($sql); //query metoden av mysqli objektet returnerar mysqli_result objektet
-print($stmt->num_rows);//(printar ut en 1) mysqli_reslut objektet har en egenskap som heter num_rows
+$result = $conn->query($sql); //query metoden av mysqli objektet returnerar mysqli_result objektet
+print($result->num_rows);//(printar ut en 1) mysqli_reslut objektet har en egenskap som heter num_rows
 
 //UNPREPARATIONSES!!!!!!!
-if($stmt->num_rows > 0){
-    while($row = $stmt->fetch_assoc())
+if($result->num_rows > 0){
+    while($row = $result->fetch_assoc())
     print("<h3>Användar namn: " . $row['username']."</h3>");
 }
