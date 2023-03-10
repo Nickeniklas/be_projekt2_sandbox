dc790f4066b499e8e5a771dedd8a099e34af5d41
Nickeniklas <savonhen@arcada.fi>
2023-03-10 17:08:45 +0200

    not working comments

--- projekt2/model_ad.php
+++ projekt2/model_ad.php
@@ -1,16 +1,32 @@
 <?php 
 $profile = test_input($_REQUEST['profile']);
-$sql = "SELECT username, realname, bio FROM profiles WHERE username=?";
+$sql = "SELECT username, realname, bio, likes FROM profiles WHERE username=?";
 $stmt = $pdoconn->prepare($sql);
 $stmt->execute([$profile]);
-$row = $stmt->fetch(PDO::fetch_assoc);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+print("<div class='profileAD'>Profile for user: <strong>" . $row['realname'] . "</strong>");
+print("<p>".$row['username']." berättar om sig själv: ".$row['bio']."</p>");
+print("<p>total likes: ".$row['likes']." </p>");
+//include "placecomment.php";
+
+?>
+ <form action="index.php" method="get">
+<input type="text" placeholder="Comment" name="comment"/>
+<input type="submit" name="submitComment" value="Place Comment"/> 
+</form>    
+<?="</div>";
+
+$id=NULL;
+$sender=1;
+$receiver=2;
+$msg = $_GET['comment']; 
+if(isset($_GET['submitComment'])){
+    $msg = $_GET['comment'];  
+    $sql = "INSERT INTO Comments (id, sender, receiver, message) VALUES (?, ?, ?, ?)";
+    $stmt= $pdoconn->prepare($sql);
+    $stmt->execute([$id, $sender, $receiver, $msg]);
+};
 ?>
 
-<?php if($row = $stmt->fetch(PDO::fetch_assoc)): ?>
-<div>                          
-    <p>Profilen för användaren: <?= $row['username']?>;</p>
-    <p>Bio: <?= $row['bio']?>;</p>
-</div>
-<?php endif; ?>
 
 
